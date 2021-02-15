9db8b5d74a614e33998c7f2cd0a2fdb8c5f8052d
Kayam Rajaram <kayamraj@gmail.com>
2021-02-15 13:46:26 -0500

    Second Commit

--- 01_python_fundamentals/01_02_seconds_years.py
+++ 01_python_fundamentals/01_02_seconds_years.py
@@ -4,3 +4,12 @@ Move the code you previously wrote to calculate how many seconds are in a year i
 Then execute it as a script to see the output printed to your console.
 
 '''
+
+DaysNum = 365
+HoursNum = 24
+MinutesNum = 60
+SecondsNum = 60
+SecondsinaYear = DaysNum * HoursNum * MinutesNum * SecondsNum
+
+print ("The number of seconds in a year is ")
+print (SecondsinaYear)
