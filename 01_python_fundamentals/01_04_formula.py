423da5e7f2ec4cc6dd24e0076f40803a61ad1452
Kayam Rajaram <kayamraj@gmail.com>
2021-02-15 13:57:27 -0500

    Final Labs

--- 01_python_fundamentals/01_04_formula.py
+++ 01_python_fundamentals/01_04_formula.py
@@ -4,3 +4,13 @@ Write the necessary code to print the result of the following formula:
 	(15.7 * 3.6 - 34.9 * 0.9) / (68.9 - 2.1)
 
 '''
+
+x = 15.7
+y = 3.6
+z = 34.9
+a = 0.9
+b = 68.9
+c = 2.1
+
+Result = (x * y - z * a)/(b - c)
+print (Result)
