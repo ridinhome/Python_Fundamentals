d776bc07eae96356013c0a1c8e5ba523cd28cd84
Kayam Rajaram <kayamraj@gmail.com>
2021-02-20 14:29:45 -0500

    Variables Labs

--- 02_basic_datatypes/1_numbers/02_01_cylinder.py
+++ 02_basic_datatypes/1_numbers/02_01_cylinder.py
@@ -4,3 +4,18 @@ of a cylinder with a radius of 3.14 and a height of 5. Print out the result.
 
 
 '''
+
+# import the math library
+import math
+
+# assign the values of the radius and height
+cylinder_radius = 3.14
+cylinder_height = 5
+
+# calculate the volume and area rounded to 2 decimal places
+cylinder_volume = round(math.pi * cylinder_height * cylinder_radius ** 2, 2)
+cylinder_area = round(2 * math.pi * cylinder_radius * cylinder_height - 2 * math.pi * cylinder_radius ** 2, 2)
+
+# print the results
+print("The area of the cylinder is ", cylinder_area)
+print("The volume of the cylinder is ", cylinder_volume)
