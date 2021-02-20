5b23bc28ca17d5c1173697d012b9a368a7289918
Kayam Rajaram <kayamraj@gmail.com>
2021-02-20 15:07:35 -0500

    Variables Lab

--- 02_basic_datatypes/1_numbers/02_05_convert.py
+++ 02_basic_datatypes/1_numbers/02_05_convert.py
@@ -10,3 +10,20 @@ Demonstrate how to:
     Take note of what information is lost when some conversions take place.
 
 '''
+
+value_a = 90
+value_b = 11.0
+
+value_a_float = float(value_a)
+print (value_a_float)
+
+value_b_float = int(value_b)
+print(value_b_float)
+
+value_c = value_a // value_b
+print(value_c)
+
+c = input("Enter a value")
+b = input("Enter another value")
+cd = float(c) * float(b)
+print(cd)
--- 02_basic_datatypes/1_numbers/02_06_invest.py
+++ 02_basic_datatypes/1_numbers/02_06_invest.py
@@ -7,3 +7,12 @@ Take in the following three values from the user:
 Print the future values to the console.
 
 '''
+
+inv_amt = int(input("Enter investment amount:"))
+int_rate = int(input("Enter an interest rate in percent:"))
+horizon = int(input("Enter number of years:"))
+
+future_value = inv_amt * (1 + int_rate / 100) ** horizon
+print("Future value is:", future_value)
+
+
